a741af8d9fdf704992623471f79d78fc2d6408cb
Nathan Tippy <nathan@tippy.name>
2025-03-10 21:11:10 -0500

    update monitor name

--- src/actor/heartbeat.rs
+++ src/actor/heartbeat.rs
@@ -4,18 +4,18 @@ use log::*;
 use steady_state::*;
 
 pub async fn run(context: SteadyContext) -> Result<(),Box<dyn Error>> {
-    let mut monitor = context.into_monitor([],[]);
+    let mut cmd = context.into_monitor([], []);
 
-    let args = monitor.args::<crate::MainArg>().expect("unable to downcast");
+    let args = cmd.args::<crate::MainArg>().expect("unable to downcast");
     let rate = Duration::from_millis(args.rate_ms);
 
     let mut count = args.beats;
-    while monitor.is_running(|| true) {
-        await_for_all!(monitor.wait_periodic(rate));
+    while cmd.is_running(|| true) {
+        await_for_all!(cmd.wait_periodic(rate));
         info!("Heartbeat {} {:?}", count, rate );
         count -= 1;
         if  count == 0 {
-            monitor.request_graph_stop();
+            cmd.request_graph_stop();
         }
     }
     Ok(())
